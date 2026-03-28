51f9b3949a2f2959618f8aa4dd43fd4ccf93b45f
Dal Zo Mung <dalzomung.mung@gmail.com>
2026-03-28 23:08:39 +0630

    Add files via upload

---
+++ 2024S_FE-B_Qs/2024S_FE-B_Q14.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double calcSim(int v1[], int v2[], int n) {
+    int i, x, y;
+    int sxx = 0;
+    int syy = 0;
+    int sxy = 0;
+
+    for (i = 0; i < n; i++) {
+        x = v1[i];
+        y = v2[i];
+		sxx = sxx + x * x;	// sxx += x * x;
+		syy = syy + y * y;	// syy += y * y;
+		sxy = sxy + x * y;	// sxy += x * y;
+    }
+
+    return (double)sxy / sqrt(sxx * syy);
+}
+
+int main() {
+    int v1[] = {2, 2, 1, 0, 4};
+    int v2[] = {3, 1, 1, 1, 2};
+    int n = 5;
+
+    double result = calcSim(v1, v2, n);
+
+    cout << "Similarity = " <<round(result*10)/10<< endl;
+
+    return 0;
+}
